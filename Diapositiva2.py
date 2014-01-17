7601f9fa5d9f2bfe6438ac648bb33206ff56f5d4
José Jácome <josejacomeb@openmailbox.org>
2014-01-17 15:19:28 -0500

    Arreglo con funciones

--- Diapositiva2.py
+++ Diapositiva2.py
@@ -13,60 +13,65 @@ apellidos =[]
 aux = 0
 longitud = int(0)
 integrantes = 0
+razon = 0
 print('Ingrese el nombre de los integrantes presione solo enter para finalizar')
 while True:
 	ingreso = input('['+str(integrantes + 1)+']: ')
 	if ingreso == '':
-        break
+		break
 	else:
 		apellidos.append(ingreso)
 		if longitud < len(apellidos[integrantes]):
 			longitud = len(apellidos[integrantes])
 		integrantes += 1
-error = ValueError
-while error == ValueError:
-	try:
+def nDiapositivas():
+	error = ValueError
+	while error == ValueError:
+		try:
+			diapositivas=int(input('Ingrese el numero de diapositivas: '))
+			error = 0
+		except ValueError:
+			error = ValueError
+			print('Ingrese un numero por favor!')
+	while(diapositivas < integrantes):
+		print('Valor invalido de diapositivas, ingrese de nuevo')
 		diapositivas=int(input('Ingrese el numero de diapositivas: '))
-		error = 0
-	except ValueError:
-		error = ValueError
-		print('Ingrese un numero por favor!')
-while(diapositivas < integrantes):
-    print('Valor invalido de diapositivas, ingrese de nuevo')
-    diapositivas=int(input('Ingrese el numero de diapositivas: '))
-if diapositivas%integrantes > 4:
-  razon=int(diapositivas/integrantes)+1
-else:
-  razon=int(diapositivas/integrantes)
-random.shuffle(apellidos)
-for i in range(0,len(apellidos)):
-    while len(apellidos[i]) < longitud:
-        apellidos[i] += " "
-    if i < integrantes - 1 :
-      apellidos[i]+=' = ' + str(aux+1) +"-"+str(aux+razon)
-    else:
-      apellidos[i]+=' = ' + str(aux+1) +"-"+str(diapositivas)
-    aux += razon
+	if diapositivas%integrantes > 4:
+		razon=int(diapositivas/integrantes)+1
+	else:
+		razon=int(diapositivas/integrantes)
+	random.shuffle(apellidos)
+	for i in range(0,len(apellidos)):
+		while len(apellidos[i]) < longitud:
+			apellidos[i] += " "
+		if i < integrantes - 1 :
+			apellidos[i]+=' = ' + str(aux+1) +"-"+str(aux+razon)
+		else:
+			apellidos[i]+=' = ' + str(aux+1) +"-"+str(diapositivas)
+		aux += razon
+		
+	#Imprime en orden el diccionario
+	apellidos.sort()
+	print(integrantes)
+	day = str(date.today().day)
+	month = str(date.today().month)
+	year = str(date.today().year)
+	hora = str(time.localtime()[3])
+	minuto = str(time.localtime()[4])
+	segundo = str(time.localtime()[5])#Crea un log para todos los integrantes
+	log = open('integrantes.log','w')
+	for i in range(0,len(apellidos)):
+		log.write(apellidos[i] + "\n");
+		print(apellidos[i])
+	log.write("\n<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>\n")
+	log.write("Numero de Diapositivas = " + str(diapositivas) + ", Numero de Integrantes = "+ str(integrantes)+"\n")
+	log.write(day+ "/" + month + "/" +year+ ","+hora+":"+minuto+":"+segundo + " Generado en python "+platform.python_version()+"\n")
+	log.write(platform.node() + " " + platform.system() + " " + platform.release() + "\n")
+	log.close()
+	print(day+ "/" + month + "/" +year+ ","+hora+":"+minuto+":"+segundo + " Generado en python"+platform.python_version())
+	print(platform.node() + " " + platform.system() + " " + platform.release())
+	print("Guardado en el archivo " + os.getcwd() + "/integrantes.log")
+def nTemas():
+	
 
-#Imprime en orden el diccionario
-apellidos.sort()
-print(integrantes)
-day = str(date.today().day)
-month = str(date.today().month)
-year = str(date.today().year)
-hora = str(time.localtime()[3])
-minuto = str(time.localtime()[4])
-segundo = str(time.localtime()[5])#Crea un log para todos los integrantes
-log = open('integrantes.log','w')
-for i in range(0,len(apellidos)):
-    log.write(apellidos[i] + "\n");
-    print(apellidos[i])
-log.write("\n<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>\n")
-log.write("Numero de Diapositivas = " + str(diapositivas) + ", Numero de Integrantes = "+ str(integrantes)+"\n")
-log.write(day+ "/" + month + "/" +year+ ","+hora+":"+minuto+":"+segundo + " Generado en python "+platform.python_version()+"\n")
-log.write(platform.node() + " " + platform.system() + " " + platform.release() + "\n")
-log.close()
-print(day+ "/" + month + "/" +year+ ","+hora+":"+minuto+":"+segundo + " Generado en python"+platform.python_version())
-print(platform.node() + " " + platform.system() + " " + platform.release())
-print("Guardado en el archivo " + os.getcwd() + "/integrantes.log")
 input()
